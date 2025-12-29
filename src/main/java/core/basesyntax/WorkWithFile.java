1dcfa823a52bf326e317783258903027ea2dabe1
Ilias <iliasaiakos@gmail.com>
2025-12-29 22:30:56 +0100

    update

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -8,7 +8,8 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class WorkWithFile {
-
+    private static final String SUPPLY_OPERATION = "supply";
+    private static final String BUY_OPERATION = "buy";
     public void getStatistic(String fromFileName, String toFileName) {
         int supply = 0;
         int buy = 0;
@@ -23,9 +24,9 @@ public class WorkWithFile {
                 if (tokens.length >= 2) {
                     String trimmedToken0 = tokens[0].trim();
                     String trimmedToken1 = tokens[1].trim();
-                    if (trimmedToken0.equals("supply")) {
+                    if (trimmedToken0.equals(SUPPLY_OPERATION)) {
                         supply += Integer.parseInt(trimmedToken1);
-                    } else if (trimmedToken0.equals("buy")) {
+                    } else if (trimmedToken0.equals(BUY_OPERATION)) {
                         buy += Integer.parseInt(trimmedToken1);
                     }
                 }
@@ -35,9 +36,8 @@ public class WorkWithFile {
                     "result," + (supply - buy);
             BufferedWriter writer = new BufferedWriter(new FileWriter(toFileName));
             writer.write(report);
-            writer.close();
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Can't read from file " + fromFileName + " or write to file " + toFileName, e);
 
         }
     }
